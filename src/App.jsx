2c5b321d56f761ce90e590b8c1775741b7e78d6a
brandonodouglas <brandondouglas247@gmail.com>
2026-04-28 22:07:25 +0100

    initial commit, landing page, and empty routine page with react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
+
+
+
+
+// My views
+function Home() {
+  const navigate = useNavigate()
+
+  return (<>
+    <section id="center">
+
+      <div>
+        <h1>⚡ Hyper-focus</h1>
+        <h2> Welcome to Brandon's productivity time saving tool. ✨</h2>
+        <h3>At Hyperfocus, we understand that money and time are both important.</h3>
+        <h4>So why budget just your MONEY and not ALSO your TIME? 🚀</h4>
+
+      </div>
+      <button
+        type="button"
+        className="counter"
+        onClick={() => navigate('/myroutines')
+        }
+      >
+        START
+      </button>
+    </section>
+
+    <div className="ticks"></div>
+    <h3>Expected release date: 1st Aug 2026. ✅</h3>
+
+    <section id="next-steps">
+
+
+    </section>
+
+    <div className="ticks"></div>
+    <section id="spacer"></section>
+    <p>Coded by @brandonodouglas on github</p>
+    <p>Powered by the MERN stack.</p>
+  </>)
+}
+
+function About() {
+  return <h1>About Page</h1>;
+}
+
+function Contact() {
+  return <h1>Contact Page</h1>
+}
+
+function MyRoutines() {
+  return (<>
+  <p>You currently have zero routines. Enter some tasks and subtasks below to get started.</p>
+  
+  </>  
+)
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      {/* Navigation */}
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/myroutines" element={<MyRoutines />} />
+      </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App
